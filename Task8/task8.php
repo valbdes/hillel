4de0fe96332892ca7c5f7ff5342b73d4d97ea2db
valbdes <valbdesik@gmail.com>
2020-04-05 13:06:33 +0300

    completed

---
+++ Task8/task8.php
@@ -0,0 +1,49 @@
+<?php
+
+error_reporting(E_ALL);
+error_reporting(-1);
+ini_set('error_reporting', E_ALL);
+
+class CsvIterator implements Iterator
+{
+    private $i = 0;
+    private $csv;
+
+    public function __construct($csv)
+    {
+        $this->csv = file($csv);
+        
+    }
+
+    public function rewind()
+    {
+        $this->i = 0;
+    }
+
+    public function current()
+    {
+        $current = explode(',', $this->csv[$this->i]);
+        return '<pre>' . print_r($current, true) . '</pre>';
+    }
+
+    public function key()
+    {
+        return $this->i;
+    }
+
+    public function next()
+    {
+        ++$this->i;
+    }
+
+    public function valid()
+    {
+        return isset($this->csv[$this->i]);
+    }
+}
+
+$csv = new CsvIterator(__DIR__ . '/cats.csv');
+
+foreach ($csv as $key => $row) {
+    print_r($row);
+}
