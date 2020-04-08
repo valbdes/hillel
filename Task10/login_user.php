062718a6cf604b0d793e93d328ccbdb8ce9f2a4d
valbdes <valbdesik@gmail.com>
2020-04-08 23:35:11 +0300

    in process

--- Task10/login_user.php
+++ Task10/login_user.php
@@ -5,9 +5,13 @@ error_reporting(-1);
 ini_set('error_reporting', E_ALL);
 $userData = $_POST;
 $namePatt = '/.{1,15}/m';
+$passPatt = '/[a-zA-Zа-яА-Я,\-_\d]{8,}/m';
 
 if (!preg_match($namePatt, $userData['login'])) {
     echo 'Hello' . $userData['login'];
 } else {
     echo 'Max lengh of the name 15 letters';
+};
+if (preg_match($passPatt, $userData['password'])) {
+    echo 'incorrect password';
 };
